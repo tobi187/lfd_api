9fc47f4996ea5e22582284f6fb9035bd3d4bee89
Tobias Fischer <61592216+tobi187@users.noreply.github.com>
2022-11-03 17:21:01 +0100

    changed app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import os
 from services.files import validate_file, store_file_very_unsecure
-from services.db import try_find
+from services.db import try_find, all_jobs
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from services.word import extract_data, fill_file
@@ -10,7 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 BASE_ROUTE = "/api/v1/"
-BASE_PATH = r"C:\Users\fisch\Desktop\projects\web\lfd_api\temp"
+BASE_PATH = r"C:\Users\fischert\Desktop\projects\home\lfd_api\temp"
 
 
 @app.route(BASE_ROUTE + "/test", methods=["POST"])
@@ -25,33 +25,48 @@ def test():
     return jsonify({"status": 400, "message": "only POST allowed"})
 
 
-@app.route(BASE_ROUTE + "init", methods=["POST"])
-def get_data():
+@app.route(BASE_ROUTE + "init")
+def get_jobs():
+    jobs = all_jobs()
+    return {"status": 200, "jobs": jobs}
+
+
+@app.route(BASE_ROUTE + "upload", methods=["POST"])
+def save_file():
     if request.method == "POST":
         data = request.get_json()
-        f_name = store_file_very_unsecure(data["files"][0])
-        points = extract_data(f_name)
+        f_name = store_file_very_unsecure(data["wordData"])
+
+        return {"status": 200, "name": f_name}
+    return jsonify({"status": 400, "message": "only POST allowed"})
+
+
+@app.route(f"{BASE_ROUTE}get-info", methods=["POST"])
+def get_context():
+    if request.method == "POST":
+        req_data = request.get_json()
+        points = extract_data(req_data["name"])
 
         if len(points) < 1:
             return jsonify({"status": 400, "message": "{{ fill }} not found"})
 
-        job_data = try_find(data["job"])
+        job_data = try_find(req_data["job"])
         if job_data["status"] != 200:
             return jsonify({"status": 400, "message": "job not found"})
 
-        return jsonify({"status": 200, "file_name": f_name, "points": points, "data": job_data["data"]})
+        return jsonify({"status": 200, "name": "dont know yet", "points": points, "data": job_data["data"]})
 
-    return jsonify({"status": 400, "message": "only POST allowed"})
+    return {"status": 400, "message": "only POST allowed"}
 
 
-@app.route(BASE_ROUTE + "append", methods=["POST"])  # type: ignore
+@app.route(BASE_ROUTE + "fill", methods=["POST"])  # type: ignore
 def prepare_download():
     data = request.get_json()
     f_name = data["fileName"]
     lfd_data = data["data"]
     fill_file(lfd_data, f_name)
 
-    return {"status": 200, "name": f_name.split(".")[0]}
+    return {"status": 200, "name": f_name}
 
 
 @app.route(BASE_ROUTE + "dl/<name>")
